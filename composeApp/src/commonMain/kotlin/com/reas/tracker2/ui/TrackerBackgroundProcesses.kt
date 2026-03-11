1bb904b4f1b2fe1e0e02bf928ac04d12b4c1ca14
reas <reas.qf0@gmail.com>
2026-03-11 14:05:29 +0300

    automatically retry connection when app is active (2)

---
+++ composeApp/src/commonMain/kotlin/com/reas/tracker2/ui/TrackerBackgroundProcesses.kt
@@ -0,0 +1,20 @@
+package com.reas.tracker2.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.reas.tracker2.network.SyncManager
+import kotlinx.coroutines.delay
+import org.koin.compose.koinInject
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+fun TrackerBackgroundProcesses() {
+    val syncManager: SyncManager = koinInject()
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            syncManager.establishConnection()
+            delay(15.seconds)
+        }
+    }
+}
